9de8095efe32bd6d4cb7112a04dae4f542a3cfa3
Nastya <nastia@bugbomb.games>
2024-05-08 18:03:03 +0300

    Task 1

--- Main/HomeWork_Task1/HomeWork_Task1.cpp
+++ Main/HomeWork_Task1/HomeWork_Task1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
-int main()
-{
-	return 0;
+using namespace std;
+int main() {
+    float first, difference;
+    int last;
+    cout << "Enter the first element of the arithmetic progression: ";
+    cin >> first;
+    cout << "Enter the common difference of the arithmetic progression: ";
+    cin >> difference;
+    cout << "Enter the number of terms in the arithmetic progression: ";
+    cin >> last;
+    cout << "Result:" << endl;
+    for (int i = 0; i < last; ++i) 
+    {
+        float term = first + i * difference;
+        cout << term << " ";
+    }
+    return 0;
 }
+
